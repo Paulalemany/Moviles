ac3582fda03110c2279cd7edc4cf4e4e38e8bebf
Paulalemany <palemany@ucm.es>
2025-09-18 19:35:15 +0200

    No suena mi botón :(

--- CuacApp/app/src/main/java/com/example/cuacapp/MainActivity.java
+++ CuacApp/app/src/main/java/com/example/cuacapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cuacapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.duck);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    /** Called when the user taps the ¡Cuac! button */
+    public void sendMesagge(View view) {
+        //Do something in reponse to button
+
+        mediaPlayer.start();
+
+    }
+
 }
