aef0d202b536ba7be9aee238efb1cdb25a5938ac
Paulalemany <palemany@ucm.es>
2025-09-24 11:18:19 +0200

    4.2

--- CuacApp/desktop/src/main/java/com/example/desktop/Main.java
+++ CuacApp/desktop/src/main/java/com/example/desktop/Main.java
@@ -1,6 +1,7 @@
 package com.example.desktop;
 
 import java.awt.Button;                 //Representa el boton
+import java.awt.FlowLayout;
 import java.awt.TextField;              //Representa el texto
 import java.awt.event.MouseListener;    //Permite escuchar los eventos de raton
 
@@ -16,6 +17,18 @@ public class Main {
         renderView.setSize(600,400);    //Damos el tamaño a la ventana
         renderView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Especificamos que hacer cuando se pulsa cerrar
         renderView.setVisible(true);    //Hacemos la ventana visible
+
+        //Creamos el FlowLayout
+        renderView.setLayout(new FlowLayout());
+
+        //Creamos los componentes que se verán
+        TextField campoTexto = new TextField(); //Entre los parentesis se puede poner el numero de columnas del ancho
+        Button boton = new Button("¡Cuac!"); //El texto es el texto que tendrá dentro el botón
+
+        //Añadimos a nuestra ventana
+        renderView.add(campoTexto);
+        renderView.add(boton);
+
     }
 
 }
