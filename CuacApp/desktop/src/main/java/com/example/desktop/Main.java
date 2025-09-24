b577ddc349a5cdd15057aead4ee0a38ce0703130
Paulalemany <palemany@ucm.es>
2025-09-24 11:28:19 +0200

    4.4

--- CuacApp/desktop/src/main/java/com/example/desktop/Main.java
+++ CuacApp/desktop/src/main/java/com/example/desktop/Main.java
@@ -1,15 +1,19 @@
 package com.example.desktop;
 
+import static java.awt.SystemColor.window;
+
 import java.awt.Button;                 //Representa el boton
 import java.awt.FlowLayout;
 import java.awt.TextField;              //Representa el texto
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;    //Permite escuchar los eventos de raton
 
 import javax.swing.JFrame;              //Crea la ventana
+import javax.swing.JOptionPane;
 
 public class Main {
 
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         //Creamos la ventana
         JFrame renderView = new JFrame("Mi CuacApp");
 
@@ -28,6 +32,25 @@ public class Main {
         //Añadimos a nuestra ventana
         renderView.add(campoTexto);
         renderView.add(boton);
+
+        //Creamos la ventana emergente
+        JOptionPane ventana = new JOptionPane();
+
+        //Anadimos el listener
+        boton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+                //Mostrar ventana emergente
+                JOptionPane.showMessageDialog(renderView, campoTexto.getText());
+            }
+
+            // Métodos obligatorios de MouseListener (aunque no los usemos)
+            @Override public void mouseClicked(MouseEvent e) {}
+            @Override public void mousePressed(MouseEvent e) {}
+            @Override public void mouseEntered(MouseEvent e) {}
+            @Override public void mouseExited(MouseEvent e) {}
+        });
+
 
     }
 
